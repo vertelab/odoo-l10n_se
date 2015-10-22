7b57fcc10d6f441c775fba91c0e946f2f60f5bb8
Andreas <karlsson_andreas93@outlook.com>
2015-10-22 14:34:43 +0200

    Added report for verificate

---
+++ l10n_se_report/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#
+#
+##############################################################################
+
+
+{
+    'name': 'l10n_se_report',
+    'version': '1.0',
+    'category': 'other',
+    'summary': 'Report for l10n_se project',
+    'author': 'Vertel AB',
+    'website': 'http://www.vertel.se',
+    'depends': ['base'],
+    'data': ['l10n_se_report_view.xml'],
+
+    'installable': True,
+    'application': False,
+    #'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
