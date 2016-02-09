8e8c3acc9ac065945d367b38d79fbc685c7976ec
Axel Pennsäter <axel.pennsater@gmail.com>
2016-02-09 17:06:57 +0100

    l10n_se_sie: Experiment with http.send_file

--- l10n_se_sie/l10n_se_sie.py
+++ l10n_se_sie/l10n_se_sie.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
+from cStringIO import StringIO
 from openerp import models, fields, api, _
 from openerp.exceptions import Warning
+from openerp import http
 import base64
 
 import logging
@@ -35,7 +36,17 @@ class account_sie(models.TransientModel):
             finally:
                 fileobj.close()
             return True
-        sie_form.write({'state': 'get', 'data': base64.b64encode(self.make_sie()) })
+            # select * from account_period where accunt_period.period_id >= p1 and accunt_period.period_id <= p2
+            
+        ## TODO: plenty of if cases to know what's selected. id is integer
+        if(sie_form.start_period.id and sie_form.stop_period.id):
+            period_ids = [p.id for p in sie_form.env['account.period'].search(['&',('id','>=',sie_form.start_period.id),('id','>=',sie_form.stop_period.id)])]
+            s = [('period_id','in',period_ids)]
+        else:
+            s = [('period_id','in',[])]
+        #raise Warning(s)
+        sie_form.write({'state': 'get', 'data': base64.b64encode(self.make_sie(search=s)) })
+        #~ sie_form.write({'state': 'get', 'data': base64.b64encode(self.make_sie()) })
         return {
             'type': 'ir.actions.act_window',
             'res_model': 'account.sie',
@@ -58,34 +69,58 @@ class account_sie(models.TransientModel):
         str = ''
         for code in account_list:
             str += '#KONTO %s\n' % code  
-        #~ raise Warning("str: %s" % str)  
-        
-        for ver in self.env['account.move'].search(search):
-            str += '#VER "" %s %s "%s" %s %s\n{\n' % (ver.name, ver.date, ver.narration, ver.create_date, ver.create_uid.login)
-            
-            for trans in ver.line_id:
-                str += '#TRANS %s {} %f %s "%s" %s %s \n' % (trans.account_id.code, trans.balance, trans.date, trans.narration, trans.quantity, trans.create_uid.login)
-            str += '}\n'    
+        #raise Warning("str: %s %s search:%s" % (str, self.env['account.move.line'].search(search),search))  
         
         #TRANS  kontonr {objektlista} belopp  transdat transtext  kvantitet   sign
         #VER    serie vernr verdatum vertext regdatum sign
-        
+    
+        for ver in self.env['account.move'].search(search):
+            str += '#VER "" %s %s "%s" %s %s\n{\n' % (ver.name, ver.date, self.fix_narration(ver.narration), ver.create_date, ver.create_uid.login)
+            #~ str += '#VER "" %s %s "%s" %s %s\n{\n' % (ver.name, ver.date, ver.narration, ver.create_date, ver.create_uid.login)
+            
+            for trans in ver.line_id:
+                str += '#TRANS %s {} %f %s "%s" %s %s \n' % (trans.account_id.code, trans.balance, trans.date, self.fix_narration(trans.name), trans.quantity, trans.create_uid.login)
+            str += '}\n'
         return str
     
     #~ @api.one
     #~ def make_sie(self,search=[]):
         #~ return self.env['account.move.line'].search(search).account_id.code
         
+    # if narration is null, return empty string instead of parsing to False
+    @api.multi
+    def fix_narration(self, narration):
+        if(narration):
+            return narration
+        else:
+            return ''
         
 class account_period(models.Model):
     _inherit = 'account.period'
-    #~ _name = 'account.period'
-
+    
     @api.multi
-    def make_sie(self,ids):
-        return self[0].env['account.sie'].make_sie([('period_id','in',ids)])
+    def do_it(self,ids):
+        periods = self.get_period(ids)
+        #raise Warning(periods)
+        _logger.warning('do_it sie %s' %base64.encodestring(periods.encode('utf-8')))
+        http.send_file(StringIO(periods.encode('utf-8')),filename='period.sie')
+        #>>> base64.b64encode(u'\xfc\xf1\xf4'.encode('utf-8'))
+        #~ return http.send_file(StringIO(self.run(self.data_to_img(getattr(o, field))).make_blob(format='jpg')), filename=field, mtime=self.get_mtime(o))
+        
+        #self.write_to_file(periods)
+    
+    def get_period(self,ids):
+        return self.env['account.sie'].make_sie([('period_id','in',ids)])
+        
+      
         
 
-
-
-
+class account_account(models.Model):
+    _inherit = 'account.account'
+    
+    @api.multi
+    def get_account(self,ids):
+        if(len(self) > 0):
+            return self[0].env['account.sie'].make_sie([('id','in',ids)])
+        else:
+            return self.env['account.sie'].make_sie([('id','in',ids)])
