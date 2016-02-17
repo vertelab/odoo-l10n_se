567b5745b2ce9914a836e9a5412811801c5ad3b1
Axel Pennsäter <axel.pennsater@gmail.com>
2016-02-17 09:28:14 +0100

    l10n_se_sie: Experimenting with account.move

--- l10n_se_sie/account.py
+++ l10n_se_sie/account.py
@@ -32,14 +32,6 @@ class account_chart_template(models.Model):
 class res_company(models.Model):
     _inherit = 'res.company'
     kptyp = fields.Char(string="Kptyp")
-    
-    #~ def kptypTest(self,ids):
-        #~ self.env['account.chart.template'].search([('kptyp','in',ids)])
-        
-       #~ super(account_chart,self).get_kptypTest(ids)
-       
-       #~ super(kptypeTest,self).get_kptyp(ids)
-       #~ super(res_company, self)._auto_init(cr, context)
        
 class account_account(models.Model):
     _inherit = 'account.account'
@@ -48,7 +40,6 @@ class account_account(models.Model):
     def export_sie(self,ids):
         account_ids = self.env['account.account'].browse(ids)
         ver_ids = self.env['account.move'].search([]).filtered(lambda ver: ver.line_id.filtered(lambda r: r.account_id.code in [a.code for a in account_ids]))
-    
         str = self.env['account.sie'].make_sie2(ver_ids)
         
 class account_fiscalyear(models.Model):
@@ -67,29 +58,16 @@ class account_journal(models.Model):
     def send_form(self):
         if len(self > 0):
             sie_form = self[0]
-        
-  
   
     def export_sie(self,ids):
 #        _logger.warning('self: %s\nids: %s' %(self,ids))
         ver_ids = self.env['account.move'].search([('journal_id','in',ids)])
  #       _logger.warning('\n%s'%ver_ids)
         self.env['account.sie'].make_sie2(ver_ids)
-        self.env['account_kptyp'].get_kptyp(ver_ids)
         
-    
-## KPTYP GREJEN två klasser. de som ska skapas. lägg på attribut
-#~ class account_kptyp(models.Model):
-    #~ _inherit = 'account.chart'
-    
-    #~ @api.multi
-    #~ def get_kptyp(self,ids):
-        #~ kptyp = self.env['account.chart'].browse(ids)
-        #~ _logger.warning('\nKPTYP STUFF PARENT\n%s'%kptyp)
-    
-#~ class account_kptyp_child(account_kptyp):
-    
-    #~ @api.multi
-    #~ def get_kptyp(self,ids):
-        #~ _logger.warning('\nGET_KPTYP CHILD\n%s'%ids)
-        #~ super(account_kptyp_child,self).get_kptyp(ids)
+class account_move(models.Model):
+    _inherit = 'account.move'
+    def export_sie(self,ids):
+        ver_ids = self.env['account.move'].search([])
+        self.env['account.sie'].make_sie2(ver_ids)
+        
