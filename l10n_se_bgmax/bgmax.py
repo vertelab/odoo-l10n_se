8e2399778a05ed2b6bac02aaac9c1f5cbfecd6b7
Anders Wallenquist <aw@leia.vertel.se>
2016-03-15 13:25:08 +0100

    l10n_se_bgmax: new iterator and "cobol"-parser installed

--- l10n_se_bgmax/bgmax.py
+++ l10n_se_bgmax/bgmax.py
@@ -31,23 +31,216 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
+class avsnitt(object):
+    def __init__(self,rec):
+        self.header = rec
+        self.footer = {}
+        self.ins = []
+        self.bet = []
+        self.type = ''
+
+    def add(self,rec):
+        self.type = rec['type']
+        if rec['type'] == '20':
+            self.type = '20'
+            self.ins.append(rec)
+        elif rec['type'] == '21':
+            self.type = '21'
+            self.bet.append(rec)
+        elif self.type == '20':
+            for r in rec.keys():
+                self.bet[-1][r] = rec[r]
+        elif self.type == '21':
+            for r in rec.keys():
+                self.ins[-1][r] = rec[r]
+            
+    def check_insbelopp(self):
+        #print "summa",sum([float(b['betbelopp'])/100 for b in self.ins])
+        #print "insbel",float(self.footer['insbelopp']) / 100
+        return float(self.footer['insbelopp']) / 100 == sum([float(b['betbelopp'])/100 for b in self.ins])
+    def check_antal_bet(self):
+        #print "antal",len(self.ins)
+        #print "antal_bet",int(self.footer['antal_bet'])
+        return len(self.ins) == int(self.footer['antal_bet'])
+
+
+class BgMaxRowParser(object):
+    """Parser for BgMax bank statement import files lines."""
+    
+    layout = {
+            '01': [ # Start post
+                ('layoutnamn',3,22),
+                ('version',23,24),
+                ('skrivdag',25,44),
+                ('testmarkering',45,45),
+                ('reserv',46,80),
+            ],
+            '05': [ # Record start
+                ('mottagarbankgiro',3,12),
+                ('mottagarplusgiro',13,22),
+                ('valuta',46,50),
+            ],
+            '15': [ # Record end / insättning
+                ('mottagarbankkonto',3,37),
+                ('betalningsdag',38,45),
+                ('inslopnummer',46,50),
+                ('insbelopp',51,68),
+                ('valuta',69,71),
+                ('antal_bet',72,79),
+                ('typ_av_ins',80,80),
+            ],
+            '20': [ # betalning
+                ('bankgiro',3,12),
+                ('referens',13,37),
+                ('betbelopp',38,55),
+                ('referenskod',56,56),
+                ('betalningskanalkod',57,57),
+                ('BGC-nummer',58,69),
+                ('avibildmarkering',70,70),
+                ('reserv',71,80),
+            ],
+            '21': [ # avdrag
+                ('bankgiro',3,12),
+                ('referens',13,37),
+                ('betbelopp',38,55),
+                ('referenskod',56,56),
+                ('betalningskanalkod',57,57),
+                ('BGC-nummer',58,69),
+                ('avibildmarkering',70,70),
+                ('avdragskod',71,71),
+            ],
+            '22': [ 
+                ('22bankgiro',3,12),
+                ('22referens',13,37),
+                ('22betbelopp',38,55),
+                ('22referenskod',56,56),
+                ('22betalningskanalkod',57,57),
+                ('22BGC-nummer',58,69),
+                ('22avibildmarkering',70,70),
+                ('22reserv',71,80),
+            ],
+            '23': [ 
+                ('23bankgiro',3,12),
+                ('23referens',13,37),
+                ('23betbelopp',38,55),
+                ('23referenskod',56,56),
+                ('23betalningskanalkod',57,57),
+                ('23BGC-nummer',58,69),
+                ('23avibildmarkering',70,70),
+                ('reserv',71,80),
+            ],
+            '25': [ 
+                ('informationstext',3,52),
+                ('reserv',53,80),
+            ],            
+            '26': [ 
+                ('betalarens_namn',3,37),
+                ('extra_namn',38,72),
+                ('reserv',73,80),
+            ],
+            '27': [ 
+                ('betalarens_adress',3,37),
+                ('betalarens_postnr',38,46),
+                ('reserv',47,80),
+            ],
+            '28': [ 
+                ('betalarens_ort',3,37),
+                ('betalarens_land',38,72),
+                ('betalarens_landkod',73,74),
+                ('reserv',75,80),
+            ],
+            '29': [ 
+                ('organisationsnummer',3,14),
+                ('reserv',15,80),
+            ],
+            '70': [ 
+                ('antal_betposter',3,10),
+                ('antal_avdrag',11,18),
+                ('antal_ref',19,26),
+                ('antal_ins',27,34),
+                ('reserv',35,80),
+            ],
+
+        }
+
+    def parse_row(self,row):
+        record = {'type': row[0:2]}
+        for name,start,stop in self.layout[record['type']]:
+            record[name] = row[start-1:stop]
+        return record
+
+
+class bgMaxIterator(BgMaxRowParser):
+    def __init__(self, data):
+        self.row = 0
+        self.data = data.splitlines()
+        self.rows = len(self.data)
+        self.header = {}
+        self.footer = {}
+        self.avsnitt = []
+    
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.row >= self.rows:
+            raise StopIteration
+        rec = self.next_rec()
+        if rec['type'] == '01':
+            self.header = rec
+            rec = self.next_rec()
+        if rec['type'] == '70':
+            self.footer = rec
+            raise StopIteration()
+        if rec['type'] == '05':
+            self.avsnitt.append(avs(rec))
+            rec = self.next_rec()
+            while rec['type'] in ['20','21','22','23','25','26','27','28','29']:
+                self.avsnitt[-1].add(rec)
+                rec = self.next_rec()                
+            if rec['type'] == '15':
+                self.avsnitt[-1].footer = rec
+            return self.avsnitt[-1]
+        return rec
+
+    def next_rec(self):
+        self.row += 1
+        return self.parse_row(self.data[self.row])
+    def check_avsnitt(self):
+        ok = True
+        for a in self.avsnitt:
+            if not (a.check_insbelopp() and a.check_antal_bet()):
+                ok = False
+        return ok
+    def check_antal_ins(self):
+        #print "antal",len(self.avsnitt)
+        #print "antal_ins",int(self.footer['antal_ins'])
+        return len(self.avsnitt) == int(self.footer['antal_ins'])
+    def check_antal_betposter(self):
+        #print "antal",sum([len(a.ins) for a in self.avsnitt])
+        #print "antal_betposter",int(self.footer['antal_betposter'])
+        return sum([len(a.ins) for a in self.avsnitt]) == int(self.footer['antal_betposter'])
+    def check(self):
+        ok = True
+        for c,result in [('avsnitt',self.check_avsnitt()),('antal_betpost',self.check_antal_betposter()),('check_antal_ins',self.check_antal_ins())]:
+            if not result:
+                print "Error in check %s" % c
+                ok = False
+        return ok
+
+
+
 class BgMaxParser(object):
     """Parser for BgMax bank statement import files."""
-
-    def __init__(self):
-        """Initialize parser - override at least header_regex.
-        This in fact uses the ING syntax, override in others."""
-        self.bgmax_type = 'General'
-        self.header_lines = 0  # Number of lines to skip
+    
+    def __init__(self, data):
+        self.row = 0
+        self.data = data.splitlines()
+        self.rows = len(self.data)
+        self.header = {}
+        self.footer = {}
+        self.avsnitt = []
         self.header_regex = '^01BGMAX'  # Start of header
-        self.footer_regex = '^70'  # Stop processing on seeing this
-        self.tag_regex = '^([0-7][0-9])(.*)'  # Start of new tag
-        self.current_statement = None
-        self.current_transaction = None
-        self.statements = []
-        self.currency = ''
-        self.header_balance = 0.0
-
 
     def is_bgmax(self, line):
         """determine if a line is the header of a statement"""
@@ -58,55 +251,29 @@ class BgMaxParser(object):
                 (line[:12], 'BgMax')
             )
 
+    def __init__(self):
+        """Initialize parser - override at least header_regex.
+        This in fact uses the ING syntax, override in others."""
+        self.bgmax_type = 'General'
+        self.header_regex = '^01BGMAX'  # Start of header
+        self.current_statement = None
+        self.current_transaction = None
+        self.statements = []
+        self.currency = ''
+        self.header_balance = 0.0
+
     def parse(self, data):
         """Parse bgmax bank statement file contents."""
+        
         self.is_bgmax(data)
-        iterator = data.replace('\r\n', '\n').split('\n').__iter__()
-        line = None
-        record_line = ''
-        try:
-            while True:
-                if not self.current_statement:
-                    self.handle_header(line, iterator)
-                line = iterator.next()
-                if not self.is_tag(line) and not self.is_footer(line):
-                    record_line += line
-                    continue
-                if record_line:
-                    self.handle_record(record_line)
-                if self.is_footer(line):
-                    self.handle_footer(line, iterator)
-                    record_line = ''
-                    continue
-                record_line = line
-        except StopIteration:
-            pass
-        if self.current_statement:
-            if record_line:
-                self.handle_record(record_line)
-                record_line = ''
+        iterator = BgMaxIterator(data)
+        
+        for avsnitt in iterator:
+            self.current_statement = BankStatement()
             self.statements.append(self.current_statement)
-            self.current_statement = None
+            
         return self.statements
 
-    def is_footer(self, line):
-        """determine if a line is the footer of a statement"""
-        return line and bool(re.match(self.footer_regex, line))
-
-    def is_tag(self, line):
-        """determine if a line has a tag"""
-        return line and bool(re.match(self.tag_regex, line))
-
-    def handle_header(self, dummy_line, iterator):
-        """skip header lines, create current statement"""
-        for dummy_i in range(self.header_lines):
-            iterator.next()
-        self.current_statement = BankStatement()
-
-    def handle_footer(self, dummy_line, dummy_iterator):
-        """add current statement to list, reset state"""
-        self.statements.append(self.current_statement)
-        self.current_statement = None
 
     def handle_record(self, line):
         """find a function to handle the record represented by line"""
@@ -120,19 +287,6 @@ class BgMaxParser(object):
             return
         handler = getattr(self, 'handle_tk%s' % re.match(self.tag_regex, line).group(1))
         handler(line)
-
-    def handle_tag_20(self, data):
-        """Contains unique ? message ID"""
-        pass
-
-    def handle_tag_25(self, data):
-        """Handle tag 25: local bank account information."""
-        data = data.replace('EUR', '').replace('.', '').strip()
-        self.current_statement.local_account = data
-
-    def handle_tag_28C(self, data):
-        """Sequence number within batch - normally only zeroes."""
-        pass
 
     def handle_tag_60F(self, data):
         """get start balance and currency"""
@@ -180,60 +334,3 @@ class BgMaxParser(object):
                 stmt.local_account,
                 stmt.date.strftime('%Y-%m-%d'),
             )
-    def handle_tk01(self, data):
-        """header"""
-        self.current_statement = BankStatement()
-        
-    def handle_tk05(self, data):
-        """öppningspost"""
-        self.currency = data[22:25]
-        self.header_balance = float(data[3:20])
-        _logger.error('balance %s currency %s' % (self.header_balance,self.currency))
-        
-        pass
-    def handle_tk15(self, data):
-        """insättning"""
-        stmt = self.current_statement
-        stmt.end_balance = str2amount(data[0], data[10:])
-        stmt.date = datetime.strptime(data[1:7], '%y%m%d')
-        
-    def handle_tk20(self, data):
-        """betalning/avdragspost"""
-        _logger.error('betalning %s ' % self.current_statement)
-        transaction = self.current_statement.create_transaction()
-        _logger.error('transaction I %s ' % transaction)
-        #{'unique_import_id': '0001', 'name': False, 'partner_name': False, 'amount': 0.0, 'account_number': False, 'date': False, 'ref': False}
-        self.current_transaction = transaction
-        
-        transaction.execution_date = datetime.strptime(data[:6], '%y%m%d')
-        transaction.value_date = datetime.strptime(data[:6], '%y%m%d')
-        transaction.name = data[12:32]
-        _logger.error('transaction %s ' % transaction)
-    def handle_tk21(self, data):
-        """betalning/avdragspost"""
-        pass
-    def handle_tk22(self, data):
-        """extra referensnummer"""
-        pass
-    def handle_tk23(self, data):
-        """extra referensnummer"""
-        pass
-    def handle_tk25(self, data):
-        """informationspost"""
-        pass
-    def handle_tk26(self, data):
-        """namnpost"""
-        pass
-    def handle_tk27(self, data):
-        """adresspost 1"""
-        pass
-    def handle_tk28(self, data):
-        """adresspost 2"""
-        pass
-    def handle_tk29(self, data):
-        """organisationsnummer"""
-        pass
-    def handle_tk70(self, data):
-        """slutpost"""
-        pass
-
