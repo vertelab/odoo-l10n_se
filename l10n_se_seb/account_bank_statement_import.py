8982951c7b86ec5e22365088d2b28574d52c2b8f
Anders Wallenquist <aw@leia.vertel.se>
2016-06-29 17:27:14 +0200

    l10n_seb: new module

--- l10n_se_seb/account_bank_statement_import.py
+++ l10n_se_seb/account_bank_statement_import.py
@@ -20,7 +20,7 @@
 ##############################################################################
 import logging
 from openerp import models, _
-from .swedbank import SwedbankTransaktionsrapport as Parser
+from .seb import SEBTransaktionsrapport as Parser
 import cStringIO
 
 _logger = logging.getLogger(__name__)
@@ -33,76 +33,12 @@ class AccountBankStatementImport(models.TransientModel):
     def _parse_file(self, cr, uid, data_file, context=None):
         """Parse a Swedbank transaktionsrapport  file."""
         try:
-            _logger.debug("Try parsing with swedbank_transaktioner.")
+            _logger.debug("Try parsing with seb_transaktioner.")
             parser = Parser(data_file)
-            swedbank = parser.parse()
+            seb = parser.parse()
         except ValueError:
-            # Not a Swedbank file, returning super will call next candidate:
-            _logger.error("Statement file was not a Swedbank Transaktionsrapport file.",exc_info=True)
-            return super(AccountBankStatementImport, self)._parse_file(
-                cr, uid, data_file, context=context)
-
-
-#        bankstatement = BankStatement()
-#        bankstatement.local_currency = avsnitt.header.get('valuta').strip() or avsnitt.footer.get('valuta').strip()
-#        bankstatement.local_account = str(int(avsnitt.header.get('mottagarplusgiro', '').strip() or avsnitt.header.get('mottagarbankgiro', '').strip()))
-        transactions = []
-        total_amt = 0.00
-        try:
-            for transaction in swedbank:
-                bank_account_id = partner_id = False
-                
-                if transaction['referens']:
-                    banks = self.pool['res.partner.bank'].search(cr,uid,
-                        [('owner_name', '=', transaction['referens'])], limit=1)
-                    if banks:
-                        bank_account = self.browse(cr,uid,banks[0])
-                        bank_account_id = bank_account.id
-                        partner_id = bank_account.partner_id.id
-                vals_line = {
-                    'date': transaction['bokfdag'],  # bokfdag, transdag, valutadag
-                    'name': transaction['referens'] + (
-                        transaction['text'] and ': ' + transaction['text'] or ''),
-                    'ref': transaction['radnr'],
-                    'amount': transaction['belopp'],
-                    'unique_import_id': transaction['radnr'],
-                    'bank_account_id': bank_account_id or None,
-                    'partner_id': partner_id or None,
-                }
-                if not vals_line['name']:
-                    vals_line['name'] = transaction['produkt'].capitalize()
-                total_amt += float(transaction['belopp'])
-                transactions.append(vals_line)
-        except Exception, e:
-            raise Warning(_(
-                "The following problem occurred during import. "
-                "The file might not be valid.\n\n %s" % e.message
-            ))
-
-        vals_bank_statement = {
-            'name': swedbank.account.name,
-            'transactions': transactions,
-            'balance_start': swedbank.account.balance_start,
-            'balance_end_real':
-                float(swedbank.account.balance_start) + total_amt,
-        }
-        return swedbank.account.currency, swedbank.account.number, [
-            vals_bank_statement]
-
-
-class AccountBankSwishImport(models.TransientModel):
-    """Add process_bgmax method to account.bank.statement.import."""
-    _inherit = 'account.bank.statement.import'
-
-    def _parse_file(self, cr, uid, data_file, context=None):
-        """Parse a Swedbank swish  file."""
-        try:
-            parser = Parser(data_file)
-            _logger.debug("Try parsing with swedbank_swish.")
-            swedbank = parser.parse()
-        except ValueError:
-            # Not a Swedbank file, returning super will call next candidate:
-            _logger.debug("Statement file was not a Swedbank Swish file.",
+            # Not a SEB file, returning super will call next candidate:
+            _logger.debug("Statement file was not a SEB Kontohändelse file.",
                           exc_info=True)
             return super(AccountBankStatementImport, self)._parse_file(
                 cr, uid, data_file, context=context)
@@ -114,7 +50,7 @@ class AccountBankSwishImport(models.TransientModel):
         transactions = []
         total_amt = 0.00
         try:
-            for transaction in swedbank:
+            for transaction in seb:
                 bank_account_id = partner_id = False
                 
                 if transaction['referens']:
@@ -145,13 +81,14 @@ class AccountBankSwishImport(models.TransientModel):
             ))
 
         vals_bank_statement = {
-            'name': swedbank.account.routing_number,
+            'name': seb.account.name,
             'transactions': transactions,
-            'balance_start': swedbank.account.balance_start,
+            'balance_start': seb.account.balance_start,
             'balance_end_real':
-                float(swedbank.account.balance_start) + total_amt,
+                float(seb.account.balance_start) + total_amt,
         }
-        return swedbank.account.currency, swedbank.account.number, [
+        return seb.account.currency, seb.account.number, [
             vals_bank_statement]
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- l10n_se_seb/seb.py
+++ l10n_se_seb/seb.py
@@ -29,17 +29,18 @@ from xlrd import open_workbook, XLRDError
 from xlrd.book import Book
 from xlrd.sheet import Sheet
 
-class SwedbankTransaktionsrapport(object):
+class SEBTransaktionsrapport(object):
     """Parser for Swedbank Transaktionsrapport import files."""
     
     def __init__(self, data_file):
         self.row = 0
         try:
-            self.data = open_workbook(file_contents=data_file).sheet_by_name('Transaktionsrapport')
+            #~ self.data_file = open_workbook(file_contents=data_file)
+            self.data = open_workbook(file_contents=data_file).sheet_by_index(0)
         except XLRDError, e:
-            raise ValueError(e)                
+            _logger.error(u'Could not read file (SEB Kontohändelser.xlsx)')
+            raise ValueError(e)  
         self.rows = self.data.nrows - 3
-        _logger.error('Row 2 %s' % self.data.row(1))
         self.header = [c.value.lower() for c in self.data.row(1)]
         self.balance_start = 0.0
         self.balance_end_real = 0.0
@@ -48,41 +49,36 @@ class SwedbankTransaktionsrapport(object):
 
     def parse(self):
         """Parse swedbank transaktionsrapport bank statement file contents."""
-        if not self.data.cell(0,0).value[:21] == '* Transaktionsrapport':
-            raise ValueError('This is not a Swedbank Transaktionsrapport')
+        if not (self.data.cell(0,0).value[:14] == u'Företagsnamn:' and self.data.cell(0,3).value[:12] == u'Sökbegrepp:'):
+            _logger.error(u'Row 0 %s (was looking for Företagsnamn)' % self.data.row(0))
+            raise ValueError('This is not a SEB Transaktionsrapport')
             
-        self.balance_start = float(self.data.cell(3,11).value)
-        self.balance_end_real = float(self.data.cell(self.rows,11).value)
+        self.balance_start = float(self.data.cell(self.rows,5).value - self.data.cell(self.rows,4).value)
+        self.balance_end_real = float(self.data.cell(6,1).value)
         
-        header = {
-            'valutadag': 'date',
-            'referens': 'payee',
-            'text': 'memo',
-            'belopp': 'amount',
-            }
              
         #_logger.error('t: %s' % [t.keys() for t in SwedbankIterator(self.data)])
         #raise Warning([n for n in [t.keys() for t in SwedbankIterator(self.data)]])
         #return {header.get(n,n): t[n] for n in [t.keys() for t in SwedbankIterator(self.data)]}
-        return SwedbankIterator(self.data)
+        return SEBIterator(self.data)
         
         
 class account(object):
     pass
 
-class SwedbankIterator(object):
+class SEBIterator(object):
     def __init__(self, data):
         self.row = 0
         self.data = data
         self.rows = data.nrows - 3
         self.header = [c.value.lower() for c in data.row(1)]
         self.account = account()
-        self.account.routing_number = self.data.row(3)[2].value
-        self.account.balance_start = self.data.row(3)[11].value
-        self.account.balance_end = self.data.row(data.nrows-1)[11].value
-        self.account.currency = self.data.row(3)[4].value
-        self.account.number = self.data.row(3)[2].value
-        self.account.name = self.data.cell(0,0).value
+#        self.account.routing_number = self.data.row(3)[2].value
+        self.account.balance_start = float(self.data.cell(self.rows,5).value - self.data.cell(self.rows,4).value)
+        self.account.balance_end = float(self.data.cell(6,1).value)
+        self.account.currency = 'SEK'
+        self.account.number = self.data.cell(6,0).value
+        self.account.name = self.data.cell(0,0).value[15:30]
         
     
     def __iter__(self):
@@ -94,41 +90,5 @@ class SwedbankIterator(object):
         r = self.data.row(self.row + 3)
         self.row += 1
         return {self.header[n]: r[n].value for n in range(len(self.header))}
-
-
-class SwedbankSwish(object):
-    """Parser for Swedbank Swish import files."""
-    
-    def __init__(self, data_file):
-        self.row = 0
-        self.data = open_workbook(file_contents=data_file).sheet_by_name('Swish-rapport')
-        self.rows = self.data.nrows - 3
-        _logger.error('Row 2 %s' % self.data.row(1))
-        self.header = [c.value.lower() for c in self.data.row(1)]
-        self.balance_start = 0.0
-        self.balance_end_real = 0.0
-        self.balance_end = 0.0
-        
-
-    def parse(self):
-        """Parse swedbank transaktionsrapport bank statement file contents."""
-        if not self.data.cell(0,0).value[:15] == '* Swish-rapport':
-            raise ValueError('This is not a Swedbank Swish-rapport')
-            
-        self.balance_start = float(self.data.cell(3,11).value)
-        self.balance_end_real = float(self.data.cell(self.rows,11).value)
-        
-        header = {
-            'valutadag': 'date',
-            'referens': 'payee',
-            'text': 'memo',
-            'belopp': 'amount',
-            }
-             
-        #_logger.error('t: %s' % [t.keys() for t in SwedbankIterator(self.data)])
-        #raise Warning([n for n in [t.keys() for t in SwedbankIterator(self.data)]])
-        #return {header.get(n,n): t[n] for n in [t.keys() for t in SwedbankIterator(self.data)]}
-        return SwedbankIterator(self.data)
-
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
