b5f09fd5a609abf8ff497e277073a0cb8b771a67
Haojun Zou <apollo_zhj@msn.com>
2018-04-03 17:44:12 +0200

    l10n_se_report: new wizard to create fiscal year opening entries

--- l10n_se_report/wizard/year_end.py
+++ l10n_se_report/wizard/year_end.py
@@ -76,7 +76,7 @@ class year_end_wizard(models.TransientModel):
             })
             # balance with trial balance
             self.env['account.move.line'].create({
-                'name': ref,
+                'name': year_result_2099.name,
                 'account_id': year_result_2099.id,
                 'debit': 0.0,
                 'credit': trba,
@@ -84,7 +84,7 @@ class year_end_wizard(models.TransientModel):
             })
             # balance with income statement
             self.env['account.move.line'].create({
-                'name': ref,
+                'name': year_result_8999.name,
                 'account_id': year_result_8999.id,
                 'debit': inst,
                 'credit': 0.0,
@@ -100,3 +100,74 @@ class year_end_wizard(models.TransientModel):
                 'target': 'current',
                 'context': {}
             }
+
+
+class account_initial_entry(models.TransientModel):
+    _name = 'account.initial.entry'
+
+    fy_id = fields.Many2one(comodel_name='account.fiscalyear', string='Fiscal Year to close', required=True, help="Select a Fiscal year to close")
+    fy2_id = fields.Many2one(comodel_name='account.fiscalyear', string='New Fiscal Year', required=True)
+    journal_id = fields.Many2one(comodel_name='account.journal', string='Opening Entries Journal', domain="[('type','=','general')]", required=True, help='The best practice here is to use a journal dedicated to contain the opening entries of all fiscal years. Note that you should define it with default debit/credit accounts, of type \'situation\' and with a centralized counterpart.')
+    period_id = fields.Many2one(comodel_name='account.period', string='Opening Entries Period', required=True)
+    report_name = fields.Char(string='Name of new entries', required=True, help="Give name of the new entries")
+    equity = fields.Many2one(comodel_name='account.account', string='Equity', required=True)
+    target_move = fields.Selection(selection=[('posted', 'All Posted Entries'), ('all', 'All Entries')], string='Target Moves')
+
+    @api.one
+    @api.onchange('equity')
+    def check_equity(self):
+        if self.equity.type in ['view', 'consolidation', 'closed']:
+            raise Warning(_('You cannot create journal items on an account of type view or consolidation.'))
+
+    @api.multi
+    def create_initial_entry(self):
+        start_period = self.env['account.period'].search([('fiscalyear_id', '=', self.fy_id.id), ('date_start', '=', self.fy_id.date_start), ('special', '=', True)])
+        end_period = self.env['account.period'].search([('fiscalyear_id', '=', self.fy_id.id), ('date_stop', '=', self.fy_id.date_stop)])
+        accounts = self.env['account.account'].with_context({'period_from': start_period.id, 'period_to': end_period.id, 'state': self.target_move}).search([('type', 'not in', ['view', 'consolidation', 'closed']), ('user_type.report_type', 'in', ['asset', 'liability']), ('user_type.close_method', '!=', 'none')])
+        #1. accounts with defferal method == 'unreconciled'
+        accounts_unreconciled = accounts.filtered(lambda a: a.user_type.close_method == 'unreconciled' and a.balance != 0.0)
+        #2. accounts with defferal method == 'detail'
+        accounts_detail = accounts.filtered(lambda a: a.user_type.close_method == 'detail' and a.balance != 0.0)
+        #3. accounts with defferal method == 'balance'
+        accounts_balance = accounts.filtered(lambda a: a.user_type.close_method == 'balance' and a.balance != 0.0)
+        entry = self.env['account.move'].create({
+            'journal_id': self.journal_id.id,
+            'period_id': self.period_id.id,
+            'date': self.period_id.date_stop,
+            'ref': self.report_name,
+        })
+        debit = 0.0
+        credit = 0.0
+        account_list = []
+        for acc in accounts_unreconciled | accounts_detail | accounts_balance:
+            account_list.append({
+                'name': acc.name,
+                'account_id': acc.id,
+                'debit': abs(float("{0:.2f}".format(acc.balance))) if acc.debit > acc.credit else 0.0,
+                'credit': abs(float("{0:.2f}".format(acc.balance))) if acc.credit > acc.debit else 0.0,
+            })
+            if acc.debit > acc.credit:
+                debit += acc.balance
+            else:
+                credit += acc.balance
+        for value in account_list:
+            value.update({'move_id': entry.id})
+            self.env['account.move.line'].create(value)
+        if debit != credit:
+            self.env['account.move.line'].create({
+                'name': self.equity.name,
+                'account_id': self.equity.id,
+                'debit': abs(float("{0:.2f}".format(debit+credit))) if debit+credit < 0.0 else 0.0,
+                'credit': abs(float("{0:.2f}".format(debit+credit))) if debit+credit > 0.0 else 0.0,
+                'move_id': entry.id
+            })
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref('account.view_move_form').id,
+            'res_id': entry.id,
+            'target': 'current',
+            'context': {}
+        }
