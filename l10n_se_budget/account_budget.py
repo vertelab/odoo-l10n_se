2ec7e17e965ae196766e947cda0ff78af11ff16f
Anders Wallenquist <anders.wallenquist@vertel.se>
2018-06-15 17:23:09 +0200

    l10n_se_budget: new module

---
+++ l10n_se_budget/account_budget.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2013-2018 Vertel AB <http://vertel.se>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from odoo import api, fields, models, _
+from odoo.tools import ustr
+from odoo.exceptions import UserError
+
+# ---------------------------------------------------------
+# Budgets
+# ---------------------------------------------------------
+class AccountBudgetPost(models.Model):
+    _inherit = "account.budget.post"
+
+    user_type_ids = fields.Many2many('account.account.type', string='Account type',domain=[('deprecated', '=', False)])
+
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
