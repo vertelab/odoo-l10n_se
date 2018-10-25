dbefbe93204ea3dc556f1dc61104987dbc4fcd83
Haojun Zou <apollo_zhj@msn.com>
2018-10-25 16:15:46 +0200

    l10n_se_paypal: new module

---
+++ l10n_se_paypal/__init__.py
@@ -0,0 +1,2 @@
+import account
+import account_bank_statement_import
---
+++ l10n_se_paypal/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Enterprise Management Solution, third party addon
+#    Copyright (C) 2018- Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'PayPal Format Bank Statements Import',
+    'version': '10.0.0.1',
+    'license': 'AGPL-3',
+    'author': ' Vertel AB',
+    'website': 'http://vertel.se',
+    'category': 'Banking addons',
+    'depends': ['l10n_se_account_bank_statement_import'],
+    'external_dependencies': {
+        'python': ['xlrd'],
+    },
+    'data': ['account_journal_view.xml'],
+    'installable': 'True',
+    'application': 'False',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
