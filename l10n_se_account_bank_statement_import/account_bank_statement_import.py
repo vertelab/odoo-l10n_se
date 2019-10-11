ea033beddbb583dd2d2959964f08bada969927bc
Anders Wallenquist <anders.wallenquist@vertel.se>
2019-10-11 08:14:53 +0200

    l10n_se_account_bank_statement_import: filter not statement_id

--- l10n_se_account_bank_statement_import/account_bank_statement_import.py
+++ l10n_se_account_bank_statement_import/account_bank_statement_import.py
@@ -456,6 +456,7 @@ class AccountBankStatement(models.Model):
                 reconciled_bg = True if len(bg_statement) > 0 else False
             if not attachment and not invoice and not voucher and not reconciled_bg and not move.payment_order_id:
                 untrackable_move_ids |= move
+        _logger.warn('anders: untrackable_move_ids %s' % untrackable_move_ids.mapped('name'))
         return untrackable_move_ids
 
     # ~ @api.model
@@ -478,8 +479,90 @@ class AccountBankStatement(models.Model):
         # ~ raise Warning('Not implemented')
 
 
+    @api.multi
+    def reconciliation_widget_preprocess(self):
+        """ Get statement lines of the specified statements or all unreconciled statement lines and try to automatically reconcile them / find them a partner.
+            Return ids of statement lines left to reconcile and other data for the reconciliation widget.
+        """
+        res = super(AccountBankStatement,self).reconciliation_widget_preprocess()
+        lines = self.env['account.bank.statement.line'].browse(res['st_lines_ids'])
+        _logger.warn('anders: XXX %s' % lines.mapped('name'))
+        # ~ raise Warning(self.mapped('line_ids.journal_entry_ids.name'))
+        # ~ raise Warning('Hello %s' % lines)
+        
+        statements = self
+        bsl_obj = self.env['account.bank.statement.line']
+        # NB : The field account_id can be used at the statement line creation/import to avoid the reconciliation process on it later on,
+        # this is why we filter out statements lines where account_id is set
+
+        sql_query = """SELECT stl.id 
+                        FROM account_bank_statement_line stl  
+                        WHERE account_id IS NULL AND not exists (select 1 from account_move m where m.statement_line_id = stl.id)
+                            AND company_id = %s
+                """
+        params = (self.env.user.company_id.id,)
+        if statements:
+            sql_query += ' AND stl.statement_id IN %s'
+            params += (tuple(statements.ids),)
+        sql_query += ' ORDER BY stl.id'
+        self.env.cr.execute(sql_query, params)
+        st_lines_left = self.env['account.bank.statement.line'].browse([line.get('id') for line in self.env.cr.dictfetchall()])
+
+        #try to assign partner to bank_statement_line
+        stl_to_assign_partner = [stl.id for stl in st_lines_left if not stl.partner_id]
+        refs = list(set([st.name for st in st_lines_left if not stl.partner_id]))
+        if st_lines_left and stl_to_assign_partner and refs\
+           and st_lines_left[0].journal_id.default_credit_account_id\
+           and st_lines_left[0].journal_id.default_debit_account_id:
+
+            sql_query = """SELECT aml.partner_id, aml.ref, stl.id
+                            FROM account_move_line aml
+                                JOIN account_account acc ON acc.id = aml.account_id
+                                JOIN account_bank_statement_line stl ON aml.ref = stl.name
+                            WHERE (aml.company_id = %s 
+                                AND aml.partner_id IS NOT NULL) 
+                                AND (
+                                    (aml.statement_id IS NULL AND aml.account_id IN %s) 
+                                    OR 
+                                    (acc.internal_type IN ('payable', 'receivable') AND aml.reconciled = false)
+                                    )
+                                AND aml.ref IN %s
+                                """
+            params = (self.env.user.company_id.id, (st_lines_left[0].journal_id.default_credit_account_id.id, st_lines_left[0].journal_id.default_debit_account_id.id), tuple(refs))
+            if statements:
+                sql_query += 'AND stl.id IN %s'
+                params += (tuple(stl_to_assign_partner),)
+            self.env.cr.execute(sql_query, params)
+            results = self.env.cr.dictfetchall()
+            st_line = self.env['account.bank.statement.line']
+            for line in results:
+                st_line.browse(line.get('id')).write({'partner_id': line.get('partner_id')})
+
+        return {
+            'st_lines_ids': st_lines_left.ids,
+            'notifications': [],
+            'statement_name': len(statements) == 1 and statements[0].name or False,
+            'num_already_reconciled_lines': 0,
+        }
+
+
+
 class AccountBankStatementLine(models.Model):
     _inherit = "account.bank.statement.line"
 
     bg_account = fields.Char(string='BG Account')
     bg_serial_number = fields.Char(string='BG Serial Number')
+
+
+    def get_move_lines_for_reconciliation(self, excluded_ids=None, str=False, offset=0, limit=None, additional_domain=None, overlook_partner=False):
+        """ Return account.move.line records which can be used for bank statement reconciliation.
+
+            :param excluded_ids:
+            :param str:
+            :param offset:
+            :param limit:
+            :param additional_domain:
+            :param overlook_partner:
+        """
+        res = super(AccountBankStatementLine,self).get_move_lines_for_reconciliation(excluded_ids, str, offset, limit, additional_domain, overlook_partner)
+        return res.filtered(lambda line: not line.statement_id)
