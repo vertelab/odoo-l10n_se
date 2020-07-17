ccd785d612dafb06ebc2e29ce225304d696ee214
Anders Wallenquist <anders.wallenquist@vertel.se>
2020-07-17 09:04:39 +0000

    l10n_se_mis: new module

---
+++ l10n_se_mis/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Enterprise Management Solution, third party addon
+#    Copyright (C) 2020- Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Swedish MIS-reports',
+    'version': '10.0.0.1.0',
+    'license': 'AGPL-3',
+    'author': ' Vertel AB',
+    'website': 'http://vertel.se',
+    'category': 'l10n',
+    'depends': ['mis_builder'],
+    'external_dependencies': {
+        'python': ['xlrd'],
+    },
+    'data': ['data/mis_financial_report.xml']
+    'installable': 'True',
+    'application': 'False',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
