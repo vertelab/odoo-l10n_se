def4eadb98f51694ae74f1a5b264d3f8fe2a9b82
Anders Wallenquist <anders.wallenquist@vertel.se>
2020-07-18 08:14:51 +0000

    l10n_se_mis: correcto9n in manifest

--- l10n_se_mis/__manifest__.py
+++ l10n_se_mis/__manifest__.py
@@ -29,7 +29,7 @@
     'external_dependencies': {
         'python': ['xlrd'],
     },
-    'data': ['data/mis_financial_report.xml']
+    'data': ['data/mis_financial_report.xml'],
     'installable': 'True',
     'application': 'False',
 }
