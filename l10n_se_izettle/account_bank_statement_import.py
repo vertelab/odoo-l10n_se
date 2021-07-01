eb8615df5515da1746a7114539535318f6354b5f
Jakob Krabbe <jakob.krabbe@vertel.se>
2021-07-01 16:01:48 +0200

    izettle: adding files, development. Ange incheckningsmeddelandet för dina ändringar. Rader som inleds

--- l10n_se_izettle/account_bank_statement_import.py
+++ l10n_se_izettle/account_bank_statement_import.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2013-2016 Vertel AB <http://vertel.se>
+#    Copyright (C) 2013-2021 Vertel AB <http://vertel.se>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
--- l10n_se_izettle/izettle.py
+++ l10n_se_izettle/izettle.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2018- Vertel AB (<http://vertel.se>).
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -34,6 +34,52 @@ import sys
 
 
 class IzettleTransaktionsrapportType(object):
+    """Parser for iZettle Kontohändelser import files."""
+
+    def __init__(self, data_file):
+        try:
+            #~ self.data_file = open_workbook(file_contents=data_file)
+            self.data = open_workbook(file_contents=data_file).sheet_by_index(0)
+        except XLRDError, e:
+            _logger.error(u'Could not read file (iZettle Kontohändelser.xlsx)')
+            raise ValueError(e)
+        if not (self.data.cell(5,0).value[:20] == u'Betalningsmottagare:' and self.data.cell(10,0).value[:21] == u'Betalningsförmedlare:'):
+            _logger.error(u'Row 0 %s (was looking for Betalningsmottagare) %s %s' % (self.data.cell(5,0).value[:20], self.data.cell(10,0).value[:21], self.data.cell(3,2)))
+            raise ValueError(u'This is not a iZettle Report')
+
+        self.nrows = self.data.nrows - 17
+        self.header = []
+        self.statements = []
+
+    def parse(self):
+        """Parse iZettle transaktionsrapport bank statement file contents type 1."""
+
+        self.account_currency = 'SEK'
+        self.header = [c.value.lower() for c in self.data.row(16)]
+        self.account_number = self.data.cell(13,2).value
+        self.name = self.data.cell(5,2).value
+
+        self.current_statement = BankStatement()
+        self.current_statement.date = fields.Date.today()
+        self.current_statement.local_currency = self.account_currency or 'SEK'
+        self.current_statement.local_account =  self.account_number
+        self.current_statement.statement_id = 'iZettle %s' % self.data.cell(3,2).value
+        self.current_statement.start_balance = 0.0
+        for t in IzettleIterator(self.data, header_row=16):
+            transaction = self.current_statement.create_transaction()
+            transaction.transferred_amount = float(t['netto'])
+            transaction.original_amount = float(t['totalt'])
+            self.current_statement.end_balance += float(t['netto'])
+            transaction.eref = int(t['kvittonummer'])
+            transaction.name = '%s %s' % (t['korttyp'].strip(),t['sista siffror'].strip())
+            transaction.note = 'Totalt: %s\nMoms: %s\nAvgift: %s\n%s %s' % (float(t['totalt']), t['moms (25.0%)'], t['avgift'], t['korttyp'].strip(), t['sista siffror'].strip())
+            transaction.value_date = t[u'datum']
+            transaction.unique_import_id = int(t['kvittonummer'])
+
+        self.statements.append(self.current_statement)
+        return self
+
+class IzettleDagrapportType(object):
     """Parser for iZettle Kontohändelser import files."""
 
     def __init__(self, data_file):
