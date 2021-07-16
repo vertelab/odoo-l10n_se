43b1eb07cc3c27ae23f20ef9a36d99546ceba756
Marcus Jakobsson <jakmar@odooutv12.vertel.se>
2021-07-16 13:40:35 +0000

    l10n_se,l10n_se_tax_report. I have added 2021 bas-konto plan which seems to be working. For tax_report i have used mis_builder to recreate the features that we had in odoo10 that were using account.financial.report. You can now the account.move that was used to calculate the momsdeklarations report. You can also get the account.move.lines for ingående och utgående moms. I have replaced the .yml files that we had the demo folder with xml instead, since odoo12 doesn't support .yml.

--- l10n_se_tax_report/__manifest__.py
+++ l10n_se_tax_report/__manifest__.py
@@ -47,7 +47,7 @@ Taxes for financial report
         'views/mis_report_view.xml',
     ],
     'demo_xml' : [
-        'demo/account_invoice.yml',
+        # ~ 'demo/account_invoice.yml',
         'demo/account_invoice.xml',
     ],
     'installable': 'True',
--- l10n_se_tax_report/models/demo_invoice.py
+++ l10n_se_tax_report/models/demo_invoice.py
@@ -6,9 +6,26 @@ class account_invoice(models.Model):
     _inherit = 'account.invoice'
     
     @api.model
-    def _demo_add_tax_lines(self,invoice_ref_id):
+    def _demo_add_tax_lines(self, invoice_ref_id):
         record = self.env.ref(invoice_ref_id)
         record._onchange_invoice_line_ids()
         
         
+class ir_config_parameter(models.Model):
+    _inherit = 'ir.config_parameter'
+    
+    @api.model
+    def _set_value_to_ir_config_parameter(self, ir_config_parameter_ref_id):
+        record = self.env.ref(ir_config_parameter_ref_id)
+        record.value = self.env.ref('l10n_se_tax_report.moms_journal').id
 
+class res_partner(models.Model):
+    _inherit = 'res.partner'
+    
+    @api.model
+    def _set_property_account_position_id(self, res_partner_ref_id, region_ref):
+        res_partner_record = self.env.ref(res_partner_ref_id)
+        region_record = self.env.ref(region_ref)
+        # ~ This if case seems pointless but im just doing what the old account_invoice.yml file did.
+        if  res_partner_record.property_account_position_id !=  region_record:
+            res_partner_record.property_account_position_id = region_record
