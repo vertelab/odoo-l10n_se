cec35be706468262f579c225f2b87535c1647756
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-28 11:16:50 +0200

    Update __manifest__.py

--- l10n_se_mis/__manifest__.py
+++ l10n_se_mis/__manifest__.py
@@ -25,12 +25,12 @@
     'author': 'Vertel AB',
     'contributor': '',
     'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-',
+    'repository': 'https://github.com/vertelab/odoo-l10n_Se',
     'category': 'Accounting',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/',
+    'website': 'https://vertel.se/apps/l10n_se',
     'description': """
     
     """,
