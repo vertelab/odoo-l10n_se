b5c0a9117925f72daa61c02548a883dd088dee52
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-28 11:13:13 +0200

    Update __manifest__.py

--- l10n_se_mis/__manifest__.py
+++ l10n_se_mis/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2020- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,16 +15,25 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
-    'name': 'Swedish MIS-reports',
-    'version': '12.0.1.0',
+    'name': 'l10n_se: Swedish MIS-reports',
+    'summary': 'Momsdeklaration',
+    'author': 'Vertel AB',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-',
+    'category': 'Accounting',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
-    'author': ' Vertel AB',
-    'website': 'http://vertel.se',
-    'category': 'l10n',
+    'website': 'https://vertel.se/apps/',
+    'description': """
+    
+    """,
     'depends': ['mis_builder','mis_builder_budget','account_period'],
     'external_dependencies': {
         'python': ['xlrd'],
