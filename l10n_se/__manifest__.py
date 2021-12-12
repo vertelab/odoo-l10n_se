285ca3f773b5bf069d2562d9ea34528e21335326
Fredrik Arvas <fredrik@arvas.se>
2021-12-12 20:22:02 +0100

    added municipalities

--- l10n_se/__manifest__.py
+++ l10n_se/__manifest__.py
@@ -27,7 +27,7 @@
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-l10n_se',
     'category': 'Accounting',
-    'version': '14.0.0.0.0',
+    'version': '14.0.0.1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
     'website': 'https://vertel.se/apps/l10n_se',
