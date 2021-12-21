cfb6a5440b7d6ef3aca6e02a73b0fea6a45c9eaf
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-12-21 16:54:58 +0100

    Update __manifest__.py

--- l10n_se_bgmax/__manifest__.py
+++ l10n_se_bgmax/__manifest__.py
@@ -30,7 +30,7 @@
     'version': '14.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/l10n_se',
+    'website': 'https://vertel.se/apps/l10n_se/bgmax',
     'description': """
         Reading BgMax formated files from Bankgirocentralen.
 
