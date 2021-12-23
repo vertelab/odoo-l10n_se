d6c96a90271d9898cc50c92e0914cb622434ce0f
Marcus Jakobsson <jakmar@odooutv14.vertel.se>
2021-12-23 15:28:51 +0000

    Removed some leaftovers after a merge conflict

--- l10n_se_sie/__init__.py
+++ l10n_se_sie/__init__.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 from . import models
 #import l10n_se_sie_importfile
 #~ import l10n_se_sie_period
-=======
-from . import l10n_se_sie
-from . import account
-#from . import l10n_se_sie_importfile
-#~ from . import l10n_se_sie_period
->>>>>>> 0f9fb041d3b233e66b9307ad880a35d64c61d657
