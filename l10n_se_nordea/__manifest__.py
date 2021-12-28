3e5bea38a0e04d14287f8ebdc8b1c1beb4ea84bb
Jakob Krabbe <jakob.krabbe@vertel.se>
2021-12-28 09:01:12 +0100

    posting modules to vertel.se

--- l10n_se_nordea/__manifest__.py
+++ l10n_se_nordea/__manifest__.py
@@ -1,12 +1,13 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2015-2016 Vertel AB <http://vertel.se>
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,20 +15,27 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
     'name': 'l10n_se: Nordea Format Bank Statements Import',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Module for reading Nordea bank statement files',
-    'version': '10.0.0.1.0',
-    'license': 'AGPL-3',
-    'author': 'Vertel AB',
-    'website': 'http://vertel.se',
     'category': 'Accounting',
     'description': """
         Module for reading Nordea bank statement files
     """,
+    #'sequence': '1'
+    #'images': ['images/main_screenshot.png']
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/l10n_se/nordea',
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n_se',
     'depends': ['l10n_se_account_bank_statement_import'],
     #'data': ['l10n_se_sie_view.xml','account_view.xml','l10n_se_sie_data.xml'],
 
