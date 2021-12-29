4aea02264497b53d95693868e500a925381d1702
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2021-12-29 13:44:37 +0000

    l10n_se_mynt: Added the first version of mynt parser.

---
+++ l10n_se_mynt/__manifest__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'l10n_se: Mynt',
+    'summary': 'Handles Mynt Transactions',
+    'author': 'Vertel AB',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n_se',
+    'category': 'Accounting',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/apps/l10n_se/l10n_se_mynt',
+
+    'description': """Handles Mynt Transactions
+
+
+     """,
+    'depends': ['account','account_period'],
+    'init_xml': [],
+    'data': [
+        #'data/load_account_chart_template_data.xml',
+        'wizard/load_mynt_view.xml',
+        'security/ir.model.access.csv',
+
+    ],
+    'demo': [
+        #'demo/load_account_chart_template_data.xml',
+    ],
+    'installable': 'True',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
