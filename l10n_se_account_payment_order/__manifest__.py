3d6219ea64ddffc097cb70d8433e19bbfb8620fd
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-01-03 13:12:52 +0100

    Update __manifest__.py

--- l10n_se_account_payment_order/__manifest__.py
+++ l10n_se_account_payment_order/__manifest__.py
@@ -31,7 +31,7 @@
     'version': '14.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/l10n_se/account_payment_order',
+    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n-se-account_payment_order',
     'images': ['/static/description/banner.png'], # 560x280 px.
     'description': """
 Configuration of account for payment order.
