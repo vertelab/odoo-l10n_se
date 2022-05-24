80b189bf6a3e797277f92ac0c16ba673976f8fea
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-05-24 11:59:06 +0000

    Added license to l10n_se_hr_expense

--- l10n_se_hr_expense/__manifest__.py
+++ l10n_se_hr_expense/__manifest__.py
@@ -29,7 +29,7 @@
     'category': 'Accounting',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''',
+    'license': 'AGPL-3',
     'website': 'https://vertel.se/apps/odoo-l10n-se/l10n-se-hr-expense/',
     'images': ['/static/description/banner.png'], # 560x280 px.
     'description': """This module adds a few Swedish expense products related to work trips.""",
