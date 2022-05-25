8ff90bd0d197bdd557c0c286e2dcbfb11ff5b96c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-25 13:57:06 +0200

    Update __manifest__.py

--- l10n_se_skv/__manifest__.py
+++ l10n_se_skv/__manifest__.py
@@ -30,7 +30,7 @@
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/odoo-l10n-se/l10n-se-skv',
+    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_skv',
     'images': ['/static/description/banner.png'], # 560x280 px.
     'description': """
         This module allows importing and redaing transactions from Skatteverket (to skattekontot).
