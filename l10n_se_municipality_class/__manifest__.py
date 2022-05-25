16ce37f19a6010d29f98e922a3269646894fd96d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-25 13:38:01 +0200

    Update __manifest__.py

--- l10n_se_municipality_class/__manifest__.py
+++ l10n_se_municipality_class/__manifest__.py
@@ -30,7 +30,7 @@
     'version': '14.0.0.1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n-se-municipality_class/',
+    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_municipality_class',
     'images': ['/static/description/banner.png'], # 560x280 px.
     'description': """
 	The standard hiearchy of Country states is on the second level in a country. This module adds a third level: Country -> State -> Municipality.\n
