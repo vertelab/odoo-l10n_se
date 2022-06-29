8d6d85ef0a0296c382e373a3db11f64671a9fc3a
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-06-29 16:33:42 +0200

    Update __manifest__.py

--- l10n_se_izettle/__manifest__.py
+++ l10n_se_izettle/__manifest__.py
@@ -31,7 +31,7 @@
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
     'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_izettle',
-    'images': ['/static/description/banner.png'], # 560x280 px.
+    'images': ['static/description/banner.png'], # 560x280 px.
     'depends': [
         'account_period', 
         'l10n_se_account_bank_statement_import', 
