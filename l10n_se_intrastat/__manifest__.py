de019a2ada5d0316798d2c081e603be305b535e3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-06-29 16:33:08 +0200

    Update __manifest__.py

--- l10n_se_intrastat/__manifest__.py
+++ l10n_se_intrastat/__manifest__.py
@@ -31,6 +31,7 @@
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
     'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_intrastat',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'description': """
     https://www.scb.se/lamna-uppgifter/undersokningar/intrastat-in--och-utforsel-av-varor/
     """,
