af70fc04aadd458012b4c5bfb5c1f874b5509f93
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-06-29 16:47:35 +0200

    Update __manifest__.py

--- l10n_se_klarna/__manifest__.py
+++ l10n_se_klarna/__manifest__.py
@@ -26,12 +26,12 @@
     'contributor': '',
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-l10n_se',
-    'images': ['/static/description/banner.png'], # 560x280 px.
+    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_klarna',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'category': 'Accounting',
     'version': '14.0.0.1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_klarna',
     'description': """
         Module for reading Klarna bank statement files
     """,
