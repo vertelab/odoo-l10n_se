512d83479f4f1661aa6363b2ff473b8251e06aff
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-20 16:06:14 +0200

    Update __manifest__.py

--- l10n_se_swedbank/__manifest__.py
+++ l10n_se_swedbank/__manifest__.py
@@ -21,20 +21,21 @@
 
 {
     'name': 'l10n_se: Swedbank Format Bank Statement Import',
-    'summary': 'Module for reading Swedbank bank statement files',
-    'author': 'Vertel AB',
-    'contributor': '',
-    'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-l10n_se',
-    'category': 'Accounting',
     'version': '14.0.0.1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_swedbank',
-    'images': ['static/description/banner.png'], # 560x280 px.
+    'summary': 'Module for reading Swedbank bank statement files',
+    'category': 'Accounting',
     'description': """
         The module allows importing and reading Swedbank bank statement files.
     """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_swedbank',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n_se',
     'depends': ['l10n_se_account_bank_statement_import'],
     #'data': ['l10n_se_sie_view.xml','account_view.xml','l10n_se_sie_data.xml'],
 
