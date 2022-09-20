a6ff3e1fc3ed7152769c21769035c7dca6604e3a
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-20 14:24:07 +0200

    Update __manifest__.py

--- l10n_se_handelsbanken/__manifest__.py
+++ l10n_se_handelsbanken/__manifest__.py
@@ -21,20 +21,20 @@
 
 {
     'name': 'l10n_se: Handelsbanken Format Bank Statement Import',
-    'summary': 'Module for reading Handelsbanken bank statement files',
-    'author': 'Vertel AB',
-    'contributor': '',
-    'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-l10n_se',
-    'category': 'Accounting',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_handelsbanken',
-    'images': ['static/description/banner.png'], # 560x280 px.
+    'summary': 'Module for reading Handelsbanken bank statement files',
+    'category': 'Accounting',
     'description': """
         Module for reading Handelsbanken bank statement files
     """,
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_handelsbanken',
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n_se',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'depends': ['l10n_se_account_bank_statement_import'],
     #'data': ['l10n_se_sie_view.xml','account_view.xml','l10n_se_sie_data.xml'],
     #'demo': ['l10n_se_sie_demo.xml'],
