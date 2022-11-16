c06d467cd4ea8cfbf3905415109c8d0d60afc09a
olyrhc <robin.calvin@vertel.se>
2022-11-16 10:03:08 +0000

    l10n_se_tax_report_rotrut: Added functionality to the invoice view

--- l10n_se_tax_report_rotrut/models/account_move_line.py
+++ l10n_se_tax_report_rotrut/models/account_move_line.py
@@ -4,7 +4,5 @@ from odoo import models, fields, api, _
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    # is_rotrut_id = fields.Many2one('account.move', string='is_rotrut')
-    is_rotrut = fields.Boolean(string='RotRut avdrag', related='move_id.is_rotrut')
-    rotrut_id = fields.Many2one('account.rotrut', string='Rotrut', store=True)
-    # name_id = fields.Many2one('account.rotrut', string='Utfört arbete')
+    is_rotrut = fields.Boolean(string='Rot/Rut-avdrag', related='move_id.is_rotrut')
+    rotrut_id = fields.Many2one('account.rotrut', string='Utfört arbete', store=True)
