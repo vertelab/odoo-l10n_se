4806f665595d0a977ace3de2dcb5ba178d08954e
olyrhc <robin.calvin@vertel.se>
2022-12-21 13:34:56 +0000

    l10n_se_tax_report_rotrut: Added handling of journal items for tax reduction

--- l10n_se_tax_report_rotrut/models/account_move.py
+++ l10n_se_tax_report_rotrut/models/account_move.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api, _
 import logging
+import uuid
 
 _logger = logging.getLogger(__name__)
 
@@ -8,7 +9,90 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     is_rotrut = fields.Boolean(string='Rot/Rut-avdrag')
+    rotrut_lines = fields.Monetary(string="Rot/Rut jobbkostnad")
     rotrut_amount = fields.Monetary(string="Rot/Rut avdrag", compute='_compute_amount')
+
+    #@api.onchange('line_ids','invoice_line_ids')
+    def _onchange_recompute_dynamic_rotrutlines(self):
+        #_logger.warning(uuid.uuid4())
+        for move in self:
+            #_logger.warning(f"{move=}")
+            line_vals = []
+            for line in move.line_ids:
+                #_logger.warning()
+                if line.rotrut_id:
+                    if line.account_id.code != '3221':
+                        if line.is_material:
+                            rotrut_account = self.env['account.account'].search([('code','=','3222')])
+                        else:
+                            rotrut_account = self.env['account.account'].search([('code','=','3221')])
+                        line.account_id = rotrut_account.id
+                        line.uuid = str(uuid.uuid4())
+
+                        account_type = self.env['account.account'].search([('code','=','1513')]).user_type_id
+                        _logger.warning(account_type)
+                        self.env['account.account'].search([('code','=','1513')]).user_type_id = 13
+                        _logger.warning(self.env['account.account'].search([('code','=','1513')]).user_type_id)
+
+                        line_vals.append(
+                            (0,0,{
+                            "account_id": self.env['account.account'].search([('code','=','1513')]),
+                            #"name": iline.name,
+                            #"product_uom_id": iline.product_uom_id,
+                            #"rotrut_id": iline.rotrut_id,
+                            #"invoice_rotrut_line_id": iline.id,
+                            "uuid": line.uuid,
+                            "debit": line.credit * 1.25 * line.rotrut_percent / 100,
+                            "exclude_from_invoice_tab": True,
+                            "recompute_tax_line": True,
+                        }))
+                        if line_vals:
+                            #_logger.warning(line_vals)
+                            move.update({'line_ids':line_vals})
+                            #self.env['account.account'].search([('code','=','1513')]).user_type_id = account_type
+
+                    elif line.account_id.code == '3221':
+                        for line3001 in move.line_ids:
+                            if line3001.uuid == line.uuid:
+                                line3001.debit = line.credit * 1.25 * line.rotrut_percent / 100
+                                #account_type = self.env['account.account'].search([('code','=','1513')]).user_type_id
+                                #_logger.warning(account_type)
+                                break
+
+
+
+
+    def _recompute_dynamic_lines(self, recompute_all_taxes=False, recompute_tax_base_amount=False):
+        line_obj = self.fetch_leftover_rotrut_line()
+        if line_obj:
+            line_obj.is_obsolete = True
+            self.line_ids = self.line_ids.filtered(lambda line: not line.is_obsolete)
+            self.line_ids.recompute_tax_line = True
+        self._onchange_recompute_dynamic_rotrutlines()
+        #self.env['account.account'].search([('code','=','1513')]).user_type_id = 1
+        res = super()._recompute_dynamic_lines(recompute_all_taxes,recompute_tax_base_amount)
+        return res
+
+
+
+    def fetch_leftover_rotrut_line(self):
+        line_uuid = None
+        for move in self:
+            for line3001 in move.line_ids:
+                if line3001.account_id.code == '1513':
+                    line_uuid = line3001.uuid
+                    exists = False
+                    for line3221 in move.line_ids:
+                        if line3221.account_id.code == '3221':
+                            _logger.warning(line3221.uuid)
+                            if line3221.uuid == line_uuid:
+                                _logger.warning('tjoho')
+                                exists = True
+                    if not exists:
+                        #move.update({'line_ids': [3,line3001._origin.id,0]})
+                        _logger.warning(line3001._origin.id)
+                        return line3001
+        return False
 
 
     @api.depends(
@@ -31,6 +115,7 @@ class AccountMove(models.Model):
     )
     def _compute_amount(self):
         super()._compute_amount()
+        self.rotrut_lines = 0
         self.rotrut_amount = 0
         line = None
         for move in self:
@@ -38,6 +123,7 @@ class AccountMove(models.Model):
             for line in move.line_ids:
                 if line.rotrut_id:
                     line_with_tax = line.price_subtotal * (1 + line.tax_ids.amount / 100)
+                    move.rotrut_lines += line_with_tax
                     move.rotrut_amount += (line_with_tax * line.rotrut_percent / 100)
 
             if move.is_rotrut:
