114aece6a80e7a9c395f6eb361f20a4fbbb64669
MikaelAtVertel <mikael.carlsson@vertel.se>
2023-03-01 15:54:33 +0000

    l10n_se_chemical_tax: change weight to net_weight

--- l10n_se_chemical_tax/models/product.py
+++ l10n_se_chemical_tax/models/product.py
@@ -20,6 +20,7 @@ class ProductCategory(models.Model):
     
     chemical_tax = fields.Float(string="Chemical tax", help="Chemical tax for products in this category", store=True)
     chemical_max_tax = fields.Float(string="Chemical max tax", help="Chemical max tax for products in this category")
+    elkretsen_miljoavgift = fields.Float(string="elkretsen_miljoavgift", help="environmental fee for products in this category")
 
 
 
@@ -27,15 +28,16 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
     
     
-    @api.depends("weight","categ_id.chemical_tax", "categ_id.chemical_max_tax")
+    @api.depends("net_weight","categ_id.chemical_tax", "categ_id.chemical_max_tax")
     def _chemical_compute(self):
         for product in self:
             product.chemical_max_tax = product.categ_id.chemical_max_tax
-            product.chemical_tax = product.categ_id.chemical_tax * product.weight
+            product.chemical_tax = product.categ_id.chemical_tax * product.net_weight
             if product.chemical_tax > product.chemical_max_tax:
                 product.chemical_tax = product.chemical_max_tax
      
     chemical_tax = fields.Float(string="Chemical tax", help="Computed chemical tax", compute="_chemical_compute", store=True) 
     
     chemical_max_tax = fields.Float(string="Chemical max tax", help="Chemical max tax for products in this category")
+    elkretsen_miljoavgift = fields.Float(string="elkretsen_miljoavgift", help="environmental fee for products in this category")
 
