3b26cedcd79f3a92e60db65a7d762a563231ffed
Marcus Jakobsson <jakmar@odooutv14.vertel.se>
2023-03-31 15:27:51 +0000

    l10n_se_chemical_tax: added a dependency and changed the order of the init file

--- l10n_se_chemical_tax/models/__init__.py
+++ l10n_se_chemical_tax/models/__init__.py
@@ -2,10 +2,9 @@
 
 from . import account
 from . import product
-from . import sale_report
 from . import sale_order
 from . import product_template
 #from . import account_move
 #from . import res_partner
 from . import hs_code
-
+from . import sale_report
