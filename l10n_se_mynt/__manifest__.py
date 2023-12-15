a52275dedbefdbb51e1be258f211ec84c892e223
iammiracle <miracleaayodele@gmail.com>
2023-12-15 09:44:14 +0100

    port mynt to 16.0 [not completed yet]

---
+++ l10n_se_mynt/__manifest__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'l10n_se: Mynt',
+    'version': '16.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Handles Mynt Transactions',
+    'category': 'Accounting',
+    'description': """
+    Handles Mynt Transactions
+     """,
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-l10n_se/l10n_se_mynt',
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n_se',
+    'images': ['static/description/banner.png'],  # 560x280 px.
+    'depends': [
+        'account',
+        'account_period_vrtl',
+        'mail',
+        # 'l10n_se_account_bank_statement_import',
+        'account_journal_card_type'
+    ],
+    'init_xml': [],
+    'data': [
+        'data/ir_seq_mynt.xml',
+        'security/ir.model.access.csv',
+        'views/account_journal_dashboard_views.xml',
+        'views/account_move_view.xml',
+    ],
+    'demo': [
+        #'demo/load_account_chart_template_data.xml',
+    ],
+    'installable': 'True',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ l10n_se_mynt/wizard/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import load_mynt_wizard
