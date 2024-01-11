9432bbad14f7cf932bc627c2c2c09a63dfe0032a
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-01-11 12:27:08 +0100

    Update __manifest__.py

--- mis_cash_flow/__manifest__.py
+++ mis_cash_flow/__manifest__.py
@@ -1,1 +1,46 @@
-###
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'l10n_se: MIS Cash Flow',
+    'version': '14.0.0.1.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'MIS Cash flow template.',
+    'category': 'Accounting',
+    'description': """
+    MIS Cash flow template.
+    """,
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-l10n_se/mis_cash_flow',
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-l10n_se',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'depends': ['base'],
+    'data': [
+        # 'views/res_bank_view.xml',
+       'data/mis_report.xml',
+    ],
+    'installable': 'True',
+    'application': 'False',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
