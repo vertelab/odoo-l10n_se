108c6d43210d966b5910c3fff80cb1db5c9213f4
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-12 14:52:31 +0200

    Update __manifest__.py

--- l10n_se_municipality_class/__manifest__.py
+++ l10n_se_municipality_class/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,15 +21,15 @@
 
 {
     'name': 'l10n_se: Municipality Swedish Municipalities',
-    'version': '14.0.0.1.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '1.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'A list of Swedish Municipalities',
     'category': 'Accounting',
     'description': """
 	The standard hiearchy of Country states is on the second level in a country. This module adds a third level: Country -> State -> Municipality.\n
 	\n
 	The module can be used to group res_partners and other objects if it is linked to.\n
-	An other similar module is: https://github.com/OCA/partner-contact/tree/14.0/base_location_nuts \n
+	An other similar module is: https://github.com/OCA/partner-contact/tree/16.0/base_location_nuts \n
 	\n
 	Version 14.0.0.1.0 Added a list of municipalities\n
      """,
