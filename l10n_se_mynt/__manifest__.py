0895e19bb6ddbaf1b748c189fc32d57392db3d23
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-12 14:59:55 +0200

    Update __manifest__.py

--- l10n_se_mynt/__manifest__.py
+++ l10n_se_mynt/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,8 +21,8 @@
 
 {
     'name': 'l10n_se: Mynt',
-    'version': '16.0.0.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '0.1',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Handles Mynt Transactions',
     'category': 'Accounting',
     'description': """
