1180d4a9922172e207f791fd8605ef40373b8335
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-12 14:36:01 +0200

    Update __manifest__.py

--- l10n_se_extended/__manifest__.py
+++ l10n_se_extended/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,8 +21,8 @@
 
 {
     'name': 'l10n_se: Sweden - Accounting',
-    'version': '16.0.0.1.0',
-    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '1.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Sweden - Chart of accounts',
     'category': 'Accounting/Localizations/Account Charts',
     'author': 'Vertel AB',
