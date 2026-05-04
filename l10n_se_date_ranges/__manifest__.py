0f90d6cc8d7db79cc0589a5fbcde832f17e9a02d
Marcus Jakobsson <markusjacobsson93@gmail.com>
2026-05-04 11:38:08 +0000

    T/8016 New module to generate common types of date ranges

---
+++ l10n_se_date_ranges/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'l10n_se Date Ranges',
+    'version': '18.0.1.0.0',
+    'category': 'Accounting/Localizations',
+    'summary': 'Provides fiscal year, quarter, and month date range types for Swedish localization.',
+    'description': 'Creates date range types for fiscal year, quarter, and month.',
+    'author': 'Vertel Sverige AB',
+    'website': 'vertel.se',
+    'license': 'AGPL-3',
+    'depends': ['date_range'],
+    'data': [
+        'data/date_range_type_data.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
