ffb23789c01bfbdd09037568f4968c833b91d87f
Anders Wallenquist <anders.wallenquist@vertel.se>
2026-05-19 19:40:02 +0000

    l10n_se_mis: fix resultaträkning formulas and add compact template
    - Fix Rörelseresultat: RorelseintakterLagerforandringar+Rorelsekostnader
    - Fix Finansiella poster: subtract costs instead of adding
    - Fix Resultat före skatt: ResultatEfterFinansiellaPoster+Bokslutsdispositioner
    - Fix Årets resultat: remove * -1 multiplier
    - Add Resultaträkning (kompakt) template without auto_expand_accounts

--- l10n_se_mis/__manifest__.py
+++ l10n_se_mis/__manifest__.py
@@ -41,6 +41,7 @@
     },
     'data': [
         'data/mis_financial_report.xml',
+        'data/mis_financial_report_compact.xml',
         'data/mis_momsdeklaration_report.xml',
         'data/mis_arbetsgivardeklaration_report.xml',
         'security/security.xml',
